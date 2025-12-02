d2843b7841267f567f3704f5d6ac4f3028dd550c
lank3r <76272386+lank3r@users.noreply.github.com>
2025-12-02 20:03:45 +0800

    Update [name].tsx

--- routes/api/[name].tsx
+++ routes/api/[name].tsx
@@ -3,8 +3,18 @@ import { define } from "../../utils.ts";
 export const handler = define.handlers({
   GET(ctx) {
     const name = ctx.params.name;
+    const responseData = {
+    "code": 0,
+    "message": "hello",
+    "data": {
+        "v": 1
+    }
+}
     return new Response(
-      `{"code":0,"msg":"hello","data":"hi"}`,
+      JSON.stringify(responseData), {
+      headers: { "Content-Type": "application/json" },
+      status: 200,
+    }
     );
   },
 });
